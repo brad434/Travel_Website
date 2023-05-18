86f245f61b40f5c612556eb6397fb9d3dc2d054f
Bradley Chery <Bradley434@hotmail.com>
2023-05-17 21:34:40 -0400

    update 5/17

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -34,7 +34,9 @@ const Header = () => {
                         <ul className="menu d-flex align-items-center gap-5">
                             {nav__links.map((item, index) => (
                                 <li className="nav__item" key={index}>
-                                    <NavLink to={item.path} className={navClass => navClass.isActive ? 'active__link' : ''}>{item.display}</NavLink>
+                                    <NavLink to={item.path} className={navClass => navClass.isActive ? 'active__link' : ''}>
+                                        {item.display}
+                                    </NavLink>
                                 </li>
                             ))}
                         </ul>
@@ -50,7 +52,6 @@ const Header = () => {
                             <i class="ri-menu-line"></i>
                         </span>
                     </div>
-
                 </div>
             </Row>
         </Container>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import '../styles/home.css'
+
+import { Container, Row, Col } from 'reactstrap';
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import Subtitle from '../Shared/Subtitle'
+import worldImg from './../assets/images/world.png'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            {/* ============= hero section start ============ */}
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='6'>
+                            <div className="hero__content">
+                                <div className="hero__subtitle d-flex align-item-center">
+                                    <Subtitle subtitle={'Know Before You Go'} />
+                                    <img src={worldImg} alt="" srcset="" />
+                                </div>
+                                <h1>Traveling opens the door to creating
+                                    <span className='highlight'>memories</span>
+                                </h1>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis at distinctio adipisci nihil placeat architecto saepe optio voluptatum excepturi expedita animi quis ipsum quos, aut beatae, vel, alias amet? Assumenda deleniti dolor magni quisquam voluptate modi fugiat veritatis, veniam quod aliquid fuga saepe sed, iusto, ratione temporibus et nemo dicta?</p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            {/* ============== hero section end ============== */}
+        </>
     )
 }
 
