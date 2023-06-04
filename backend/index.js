cd26e860e6c36a24d86db2600e9db2f7018a0e50
Bradley Chery <Bradley434@hotmail.com>
2023-06-03 21:45:30 -0400

    added review section

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,17 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import tourRoute from "./routes/tours.js";
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import reviewRoute from "./routes/reviews.js";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
+const corsOption = {
+  origin: true,
+  credentials: true,
+};
 
 //for testing
 // app.get("/", (req, res) => {
@@ -32,9 +39,12 @@ const connect = async () => {
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOption));
 app.use(cookieParser());
-app.use("/tours", tourRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/tours", tourRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/review", reviewRoute);
 
 app.listen(port, () => {
   connect();
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -9,15 +9,16 @@ import {
   getTourCount,
   updateTour,
 } from "./../controllers/tourController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // create new tour
-router.post("/", createTour);
+router.post("/", verifyAdmin, createTour);
 // update tour
-router.put("/:id", updateTour);
+router.put("/:id", verifyAdmin, updateTour);
 // delete tour
-router.delete("/:id", deleteTour);
+router.delete("/:id", verifyAdmin, deleteTour);
 // getSingle tour
 router.get("/:id", getSingleTour);
 // getAll tour
