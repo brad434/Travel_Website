808486ce00fcae67f46adbd54467d2eff4ffe330
Bradley Chery <Bradley434@hotmail.com>
2023-06-06 02:23:17 -0400

    updated files tourCard.jsx

--- frontend/src/Shared/SearchBar.jsx
+++ frontend/src/Shared/SearchBar.jsx
@@ -2,14 +2,18 @@ import React, { useRef } from 'react';
 import './search-bar.css';
 import { Col, Form, FormGroup } from 'reactstrap';
 
+import { BASE_URL } from './../utils/config';
+import { useNavigate } from 'react-router-dom';
+
 const SearchBar = () => {
 
     const locationRef = useRef('');
     const distanceRef = useRef(0);
     const maxGroupSizeRef = useRef(0);
+    const navigate = useNavigate()
 
-    // This function from line 12-20 is used for making the user input a value on each input but cannot oass
-    const searchHandler = () => {
+    // This function from line 12-20 is used for making the user input a value on each input but cannot pass
+    const searchHandler = async () => {
         const location = locationRef.current.value;
         const distance = distanceRef.current.value;
         const maxGroupSize = maxGroupSizeRef.current.value;
@@ -17,6 +21,14 @@ const SearchBar = () => {
         if (location === "" || distance === "" || maxGroupSize === "") {
             return alert("All fields are imported!")
         }
+
+        const res = await fetch(`${BASE_URL}/tours/search/getTourBySearch?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`);
+
+        if (!res.ok) alert("Something went wrong")
+
+        const result = await res.json();
+
+        navigate(`/tours/search/getTourBySearch?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`, { state: result.data });
     }
     return <Col lg='12'>
         <div className="search__bar">
