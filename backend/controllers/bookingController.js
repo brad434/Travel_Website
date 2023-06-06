0f0126e8e2f412b02f81c9d2f9ecfc0e6077d915
Bradley Chery <Bradley434@hotmail.com>
2023-06-06 02:22:33 -0400

    backend updating

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,5 +1,6 @@
 import Booking from "../models/Booking.js";
 
+// create new booking
 export const createBooking = async (req, res) => {
   const newBooking = new Booking(req.body);
   try {
@@ -15,5 +16,29 @@ export const createBooking = async (req, res) => {
       success: false,
       message: "Your tour was unable to be booked. Try again.",
     });
+  }
+};
+
+//get single booking
+export const getBooking = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const book = await Booking.findById(id);
+
+    res.status(202).json({ success: true, message: "successful", data: book });
+  } catch (err) {
+    res.status(404).json({ success: false, message: "not found" });
+  }
+};
+
+//get all booking
+export const getAllBooking = async (req, res) => {
+  try {
+    const books = await Booking.find();
+
+    res.status(202).json({ success: true, message: "successful", data: books });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "internal server error" });
   }
 };
